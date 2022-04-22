41e31eb5098aff44ceeef15cc310295f5d4ce255
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-22 22:41:25 +0100

    post and get people

--- src/main/java/com/omarox/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/omarox/demo/dao/FakePersonDataAccessService.java
@@ -1,17 +1,25 @@
 package com.omarox.demo.dao;
 
 import com.omarox.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
 
     private static List<Person> DB = new ArrayList<>();
 
     @Override
     public int insertPerson(UUID id, Person person) {
-        return 0;
+        DB.add(new Person(id, person.getName()));
+        return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+         return DB;
     }
 }
